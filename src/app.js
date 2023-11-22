e18ec8427027139cfd846523f5ef64d69ffa9083
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2023-11-22 19:23:32 +0000

    ultimo commit

--- src/app.js
+++ src/app.js
@@ -5,13 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(domainname) {
+window.onload = function() {
   //write your code here
   let pronoun = ["the", "our", "mine"];
   let adj = ["great", "big", "small"];
   let noun = ["jogger", "racoon", "orange"];
   let domain = [".com", ".net", ".es"];
-
   let domains = "";
 
   for (let i = 0; i < pronoun.length; i++) {
@@ -19,7 +18,12 @@ window.onload = function(domainname) {
       for (let c = 0; c < noun.length; c++) {
         for (let d = 0; d < domain.length; d++) {
           domains +=
-            "<li>" + pronoun[i] + adj[b] + noun[c] + domain[d] + "</li>";
+            "<li class='text-danger'>" +
+            pronoun[i] +
+            adj[b] +
+            noun[c] +
+            domain[d] +
+            "</li>";
         }
       }
     }
